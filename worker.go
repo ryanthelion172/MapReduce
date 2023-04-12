be5c223f023efb24bbb7703c1833cf4f90b84ec2
jake <jakejacobsen20@gmail.com>
2023-04-11 18:38:11 -0600

    added to worker(probably wrong)

--- worker.go
+++ worker.go
@@ -2,6 +2,7 @@ package mapreduce
 
 import (
 	"fmt"
+	"log"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -36,3 +37,20 @@ func reduceOutputFile(r int) string    { return fmt.Sprintf("reduce_%d_output.db
 func reducePartialFile(r int) string   { return fmt.Sprintf("reduce_%d_partial.db", r) }
 func reduceTempFile(r int) string      { return fmt.Sprintf("reduce_%d_temp.db", r) }
 func makeURL(host, file string) string { return fmt.Sprintf("http://%s/data/%s", host, file) }
+
+func (task *MapTask) Process(tempdir string, client Interface) error {
+	slice_of_output_files := make([]string, task.M)
+
+	// source_file := mapSourceFile(task.M)
+	input_file := mapInputFile(task.M)
+	for i := 0; i < task.M; i++ {
+		slice_of_output_files[i] = mapOutputFile(i, task.R)
+	}
+	err := download(input_file, tempdir)
+	if err != nil {
+		log.Printf("error downloading the file with error:%d", err)
+		return err
+	}
+	// output_files, err := splitDatabase(task.SourceHost, outputDir, outputPattern string, m int)
+	return nil
+}
