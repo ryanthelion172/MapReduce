2716255fd912e171600508b4382de1094555bff1
Ryan Crandall <Ryan.Crandall@utahtech.edu>
2023-04-13 17:22:51 -0600

    got a merge to work

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 )
@@ -21,4 +22,19 @@ func main() {
 	if err != nil {
 		log.Print(err)
 	}
+	/*go func() {
+		address := ":8080"
+		tempdir := exPath + "/temp/"
+		http.Handle("/data/", http.StripPrefix("/data", http.FileServer(http.Dir(tempdir))))
+		if err := http.ListenAndServe(address, nil); err != nil {
+			log.Printf("Error in HTTP server for %s: %v", address, err)
+		}
+	}()
+	databases := make([]string, 5)
+	databases[0] = makeURL("localhost:8080", "austen-0.db")
+	databases[1] = makeURL("localhost:8080", "austen-1.db")
+	databases[2] = makeURL("localhost:8080", "austen-2.db")
+	databases[3] = makeURL("localhost:8080", "austen-3.db")
+	databases[4] = makeURL("localhost:8080", "austen-4.db")
+	mergeDatabases(databases, "austen2.db", "temp.db")*/
 }
